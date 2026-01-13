7e261880e7c04c5cc38e04bf0d717b8f3f08e9d1
akshaydivekar0624 <akshaydivekar0624@gmail.com>
2026-01-13 20:49:13 +0530

    project structure ready and venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Akshay Divekar",
+    author_email="akshaydivekar0624@gmail.com",
+    packages=find_packages()
+)
