5222db0f60000eef910c5bd532f089f99e707d2d
akshaydivekar0624 <akshaydivekar0624@gmail.com>
2026-01-13 22:25:55 +0530

    experiment notebook added

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -1,8 +1,8 @@
 import logging
 import os
 from logging.handlers import RotatingFileHandler
-# from from_root import from_root
-from .from_root import from_root
+from from_root import from_root
+# from .from_root import from_root
 from datetime import datetime
 
 # Constants for log configuration
